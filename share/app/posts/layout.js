4d81a24e2b33f430f9c725d4132daf7bde13dfa4
renescoban <renes651@gmail.com>
2023-07-17 14:42:17 +0300

    signUp & createPost

--- share/app/posts/layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const layout = () => {
-  return (
-    <div>Scroll to Top</div>
-  )
-}
-
-export default layout
--- share/app/posts/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>page posts</div>
-  )
-}
-
-export default page
